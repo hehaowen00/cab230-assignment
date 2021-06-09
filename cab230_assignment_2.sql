013246a7d60b2ec34062bd51df77dd174b5067a8
hehaowen00 <edmundhe@outlook.com>
2021-06-09 20:58:26 +1000

    include create statement in sql dump.

--- cab230_assignment_2.sql
+++ cab230_assignment_2.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `happiness` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `happiness`;
 -- MySQL dump 10.13  Distrib 8.0.25, for macos11 (x86_64)
 --
 -- Host: 192.168.100.136    Database: happiness
@@ -86,4 +88,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-09 20:37:20
+-- Dump completed on 2021-06-09 20:58:09
