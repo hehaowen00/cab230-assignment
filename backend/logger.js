d246af88aa907612fbfee14692ab90e7666fb033
hehaowen00 <edmundhe@outlook.com>
2021-06-11 04:33:01 +1000

    update logging

--- backend/logger.js
+++ backend/logger.js
@@ -2,8 +2,9 @@ const logger = require('morgan');
 
 logger.token('req', (req, _res) => JSON.stringify(req.headers));
 logger.token('res', (_req, res) => {
-    const { statusCode, statusMessage } = res;
-    return JSON.stringify({ statusCode, statusMessage });
+    const headers = {}
+    res.getHeaderNames().map(h => headers[h] = res.getHeader(h))
+    return JSON.stringify(headers)
 });
 
 module.exports = logger('common');
